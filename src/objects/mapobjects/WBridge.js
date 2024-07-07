aab37f2643d8ad82208fbaa1b98312101cb81838
datagutten <datagutten@datagutten.net>
2024-07-07 17:18:38 +0200

    Add sounds to weak bridge

--- src/objects/mapobjects/WBridge.js
+++ src/objects/mapobjects/WBridge.js
@@ -1,7 +1,5 @@
 'use strict'
 
-import ObjectAnimation from './animation'
-
 /**
  * Weak bridge (map 8)
  * @type {{MapObject}}
@@ -11,7 +9,7 @@ const MapObject = {}
 MapObject.onCreate = function () {
   this.animationHelper.static('normal', this.opt.Direction)
 
-  this.animationSplash = this.animationHelper.add('splash', 'Splash', this.opt.Direction)
+  const splash = this.animationHelper.add('splash', 'Splash', this.opt.Direction)
   splash.onComplete.add(() => {
     this.visible = false
   }, this)
@@ -20,16 +18,25 @@ MapObject.onCreate = function () {
 MapObject.onEnterInner = function (car) {
   const weight = this.game.mulle.user.Car.getProperty('weight')
   if (weight >= 20) {
-    // TODO: Check sound and cutscene
+    // TODO: Check and add cutscene
     console.log('Car weight ', weight)
     console.log('Car too heavy, bridge broken')
     this.animations.play('splash')
     car.speed = 0
     car.stepback(9)
+    if (!this.playedSound) {
+      this.playedSound = true
+
+      const splash = this.game.mulle.playAudio(this.def.Sounds[0])
+      splash.onStop.addOnce(() => {
+        this.game.mulle.playAudio(this.def.Sounds[1])
+      })
+    }
   } else {
     console.log('Light car')
     const hasMedal = this.game.mulle.user.Car.hasMedal(6)
     if (!hasMedal) {
+      this.game.mulle.playAudio('05d010v0')
       this.game.mulle.user.Car.addMedal(6)
     }
   }
