f83394fbcdd52c7367262251ca818029e4559b0d
datagutten <datagutten@datagutten.net>
2024-07-09 17:59:18 +0200

    Award medal for big hills

--- src/objects/mapobjects/Hill.js
+++ src/objects/mapobjects/Hill.js
@@ -2,7 +2,7 @@
 
 const MapObject = {}
 
-MapObject.onEnterInner = function (car) {
+MapObject.onEnterInner = function () {
   console.log('enter hill, custom object', this)
 
   if (this.opt.HillType === '#SmallHill') {
@@ -14,6 +14,13 @@ MapObject.onEnterInner = function (car) {
     if (!this.game.mulle.user.Car.criteria.BigHill) {
       //Big hill, engine too weak
       this.game.mulle.playAudio(this.def.Sounds[1])
+    } else {
+      const medal = this.def['SetWhenDone']['Medals'][0]
+      const hasMedal = this.game.mulle.user.Car.hasMedal(medal)
+      if (!hasMedal) {
+        //TODO: Play sound
+        this.game.mulle.user.Car.addMedal(medal)
+      }
     }
   }
 }
